d07c503bf39ae6840bcc95a5a57d39157fc4a56b
Mitch Loar <mitchloarweb@gmail.com>
2016-04-16 13:21:19 -0600

    CP 4 complete, Wikis created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+         after_initialize do
+           self.role ||= "standard"
+         end
+
+         def admin?
+            role == 'admin'
+         end
+#            def ( PREMIUM AND STANDARD)
+#           role == 'admin'
+#           end
 end
